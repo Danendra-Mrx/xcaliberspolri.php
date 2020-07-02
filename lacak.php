394887e6293b44874337bea904e32e253349b292
Danendra-Mrx <65715562+Danendra-Mrx@users.noreply.github.com>
2020-07-02 16:27:40 +0700

    Create lacak.php

---
+++ lacak.php
@@ -0,0 +1,17 @@
+<?php
+function cari($nama){
+	echo "\n\n\n\n\n";
+	echo "MASUKAN NAMA PENDUDUK : ";
+	$target = trim(fgets(STDIN));
+	echo "\nLoading ";
+	for ($i=0; $i <5 ; $i++) {
+		echo ". ";
+		sleep(1);
+	}
+	echo "\n\n\nNama\t\t\tAlamat\t\t\tNIK";
+	echo "\n$target\t\tJalan Cempaka 4 Rengas\t5173964026183\n\n\n";
+}
+
+cari("asu");
+
+?>
